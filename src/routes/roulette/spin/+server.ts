d8cd4c6cb6a9f853f7711ced216ee400da0767d1
Hosokava <dworak.mikolaj@gmail.com>
2025-05-17 19:31:12 +0200

    Revert "kreci sie"
    This reverts commit 493b34c571f9f2d16fd5ba8d834f08d4a1d325b1.

--- src/routes/roulette/spin/+server.ts
+++
@@ -1,7 +0,0 @@
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = async () => {
-  const result = Math.floor(Math.random() * 37);
-  return json({ number: result });
-};

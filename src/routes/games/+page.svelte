6d1cdb2782daaa4c62e2e8d2f4ffc23e362cfaaf
Bartek Wątorek <bwatorek86@uczen.tl.krakow.pl>
2025-06-12 13:34:07 +0200

    games page

--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import games from '$lib/games/list.json';
-	// TODO: IF NOT LOGGED IN, REDIRECT TO LOGIN/MAIN PAGE
 </script>
 
-<main class="left-center flex w-fit flex-wrap justify-center gap-8 p-8 text-center">
+<main class="mx-auto mt-12 grid grid-cols-2 gap-8">
 	{#each games as game}
-		<div
-			class="relative block h-[250px] w-[250px] overflow-hidden rounded-lg bg-yellow-600 shadow-lg transition-transform"
+		<a
+			href={`/games/${game.href}`}
+			class="flex flex-col items-center justify-center transition-transform hover:scale-105"
 		>
-			<!-- <img
-				src={game.image || 'https://placehold.co/142x157'}
+			<img
+				src={game.image ? '/games/icons/' + game.image : 'https://placehold.co/142x157'}
 				alt={game.name}
-				class="absolute z-0 h-full w-full"
-				height="157"
-				width="142"
-			/> -->
-			<a href={`/games/${game.href}`} class="z-1 rounded-lg">
-				<h2 class="text-2xl text-black uppercase">{game.name}</h2>
-				<p class="text-sm">{game.desc}</p>
-			</a>
-		</div>
+				class=""
+				height="150"
+				width="150"
+			/>
+			<h2 class="mt-2 text-3xl font-bold text-white uppercase">{game.name}</h2>
+			<p class="w-3/4 text-center text-sm">{game.desc}</p>
+		</a>
 	{/each}
 </main>
